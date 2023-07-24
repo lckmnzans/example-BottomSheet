5ce1ea8e1c576f59c72e82329637ded371e5287a
lckmnzans <lckmnzans.elektro@gmail.com>
2023-07-24 11:40:43 +0700

    implementation of bottom sheet

--- app/src/main/java/com/simple/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/simple/bottomsheet/MainActivity.kt
@@ -2,10 +2,29 @@ package com.simple.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnShowBottomSheet: Button
+    private lateinit var bottomSheet: BottomSheetDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowBottomSheet = findViewById(R.id.btn_show_bottom_sheet)
+        btnShowBottomSheet.setOnClickListener {
+            bottomSheet = BottomSheetDialog(this)
+            val view = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
+            val btnClose = view.findViewById<Button>(R.id.btn_dismiss)
+
+            bottomSheet.setCancelable(false)
+            bottomSheet.setContentView(view)
+            bottomSheet.show()
+
+            btnClose.setOnClickListener {
+                bottomSheet.dismiss()
+            }
+        }
     }
 }

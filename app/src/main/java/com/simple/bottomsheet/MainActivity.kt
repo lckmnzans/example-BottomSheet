bcef15afed412c93db3b000017cfe15b03f29057
lckmnzans <lckmnzans.elektro@gmail.com>
2023-07-26 08:25:45 +0700

    create bottom sheet persist layout

--- app/src/main/java/com/simple/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/simple/bottomsheet/MainActivity.kt
@@ -2,29 +2,38 @@ package com.simple.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnShowBottomSheet: Button
-    private lateinit var bottomSheet: BottomSheetDialog
+    private lateinit var btnShowBottomSheetModal: Button
+    private lateinit var btnShowBottomSheetPersist: Button
+    private lateinit var bottomSheetModal: BottomSheetDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnShowBottomSheet = findViewById(R.id.btn_show_bottom_sheet)
-        btnShowBottomSheet.setOnClickListener {
-            bottomSheet = BottomSheetDialog(this)
-            val view = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
-            val btnClose = view.findViewById<Button>(R.id.btn_dismiss)
+        btnShowBottomSheetModal = findViewById(R.id.btn_show_bottom_sheet_modal)
+        btnShowBottomSheetPersist = findViewById(R.id.btn_show_bottom_sheet_persist)
+        bottomSheetModal = BottomSheetDialog(this)
 
-            bottomSheet.setCancelable(false)
-            bottomSheet.setContentView(view)
-            bottomSheet.show()
+        btnShowBottomSheetModal.setOnClickListener {
+            val view = layoutInflater.inflate(R.layout.bottom_sheet_modal, null)
+            val btnDismiss = view.findViewById<Button>(R.id.btn_dismiss)
 
-            btnClose.setOnClickListener {
-                bottomSheet.dismiss()
+            bottomSheetModal.setCancelable(false)
+            bottomSheetModal.setContentView(view)
+            bottomSheetModal.show()
+
+            btnDismiss.setOnClickListener {
+                bottomSheetModal.dismiss()
             }
         }
+
+        val bottomSheet: View = findViewById(R.id.bottom_sheet)
+        val bottomSheetBehavior: BottomSheetBehavior<*> = BottomSheetBehavior.from(bottomSheet)
     }
 }
